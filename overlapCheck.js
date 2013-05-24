7b4665daa2bc28ce8277c244e3fc94671c9f5ba9
Thomas Devos <devos.thom@gmail.com>
2013-05-24 17:24:49 +0200

    deadzone & ratio

--- overlapCheck.js
+++ overlapCheck.js
@@ -6,6 +6,7 @@
             elementsToCompare: null,
             container: this,
             callback: null,
+            deadZone: 0,
             type : ["horizontal", "vertical"]
         }, options );
         var $overlap=[];
@@ -15,21 +16,60 @@
             $top = $element.position().top;
             $width = $element.width();
             $height = $element.height();
-            $item = {};
-            $item.left = $left;
-            $item.right = $left + $width;
-            $item.top = $top;
-            $item.bottom = $top + $height;
+            $item = {
+                left : $left,
+                right : $left + $width,
+                top : $top,
+                bottom : $top + $height
+            };
             return $item;
+        }
+        
+        var parseFloat = function($value){
+            $value = $value.split("/");
+            return parseInt($value[0]) / parseInt($value[1]);
+        }
+        
+        var calculateHDeadZone = function(){
+            if($.isNumeric($settings.deadZone)){
+                return $settings.deadZone;
+            }else{
+                console.log($settings.elementsToCompare.width() * parseFloat($settings.deadZone));
+                return $settings.elementsToCompare.width() * parseFloat($settings.deadZone);
+            }
+        }
+        var calculateVDeadZone = function(){
+            if($.isNumeric($settings.deadZone)){
+                return $settings.deadZone;
+            }else{
+                console.log($settings.elementsToCompare.width() * parseFloat($settings.deadZone));
+                return $settings.elementsToCompare.width() * parseFloat($settings.deadZone);
+            }
+        }
+        
+        var getRatio = function($element1, $element2){
+            $item1 = getCoord($element1);
+            $item2 = getCoord($element2);
+            
+            $compare = $item1.right - $item1.left;
+            
+            $diff = $item2.right - $item1.left;
+            if($diff > $compare){
+                $diff = $item1.right - $item2.left;
+                if($diff > $compare){
+                    $diff = $compare;
+                }
+            }
+            return $diff / $compare;
         }
         
         var overlapHorizontally = function($element1, $element2){
             if($.inArray("horizontal", $settings.type) === -1)
                 return true;
-            console.log("b");
             $item1 = getCoord($element1);
             $item2 = getCoord($element2);
-            if($item1.right > $item2.left && $item1.left < $item2.right){
+            if($item1.right - calculateHDeadZone() > $item2.left 
+                    && $item1.left + calculateHDeadZone() < $item2.right){
                 return true;
             }else{
                 return false;
@@ -39,10 +79,10 @@
         var overlapVertically = function($element1, $element2){
             if($.inArray("vertical", $settings.type) === -1)
                 return true;
-            console.log("a");
             $item1 = getCoord($element1);
             $item2 = getCoord($element2);
-            if($item1.bottom > $item2.top && $item1.top < $item2.bottom){
+            if($item1.bottom - calculateVDeadZone() > $item2.top 
+                    && $item1.top  + calculateVDeadZone() < $item2.bottom){
                 return true;
             }else{
                 return false;
@@ -54,15 +94,19 @@
             $settings.container.children($settings.elements).each(function(){
                 $element2 = $(this);
                 if($element1[0] !== $element2[0]){
-                    if(overlapHorizontally($element1, $element2) && overlapVertically($element1, $element2)){
-                        $overlap.push($(this));
+                    if(overlapHorizontally($element1, $element2) 
+                            & overlapVertically($element1, $element2)){
+                        $overlap.push({
+                            element: $(this),
+                            ratio: getRatio($element1, $element2)
+                        });
                     }
                 }
             });
         });
         
-        $($overlap).each(function($id, $element){
-            $settings.callback.call(this, $element);
+        $($overlap).each(function($id, $over){
+            $settings.callback.call(this, $over.element, $over.ratio);
         });
     }
 }( jQuery ));
